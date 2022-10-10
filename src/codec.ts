822b6240b645d3d76d8da50ee87b40af15c39b8d
fryorcraken.eth <git@fryorcraken.xyz>
2022-10-10 07:22:53 -0500

    chores: log proof generated

--- src/codec.ts
+++ src/codec.ts
@@ -30,7 +30,7 @@ export class RLNEncoder implements Encoder {
   async toWire(message: Partial<Message>): Promise<Uint8Array | undefined> {
     message.contentTopic = this.contentTopic;
     message.rateLimitProof = await this.generateProof(message);
-
+    log("Proof generated", message.rateLimitProof);
     return this.encoder.toWire(message);
   }
 
@@ -42,7 +42,7 @@ export class RLNEncoder implements Encoder {
     if (!protoMessage) return;
 
     protoMessage.rateLimitProof = await this.generateProof(message);
-
+    log("Proof generated", message.rateLimitProof);
     return protoMessage;
   }
 
